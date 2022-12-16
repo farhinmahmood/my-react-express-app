615aa7b68a91f0e2af9246b33097ae37576ac835
Saida Mahmood <saida.mahmood04@gmail.com>
2022-12-16 23:54:05 +0000

    react redux component added

---
+++ src/app/components/Dashboard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Dashboard = ({groups})=>(
+    <div>
+        <h2>DashBoard</h2>
+    </div>
+)
---
+++ src/app/index.jsx
@@ -0,0 +1,14 @@
+
+import{store} from './store'
+
+import React from 'react';
+import  ReactDOM from 'react-dom';
+import { Dashboard } from './components/Dashboard'
+
+ReactDOM.render(
+    <Dashboard/>,
+    document.getElementById("app")
+);
+
+ 
+
