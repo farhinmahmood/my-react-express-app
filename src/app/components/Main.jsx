820b2178ea317389ce98cfad03d6b5b975c5f2eb
Saida Mahmood <saida.mahmood04@gmail.com>
2022-12-17 20:09:18 +0000

    Navigation and Routing

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -2,14 +2,27 @@ import React from "react";
 import {Provider} from 'react-redux';
 import {store} from '../store'
 import { ConnectedDashBoard } from "./Dashboard";
+import {Router,Route} from 'react-router-dom';
+import {history} from '../store/history';
+import { ConnectedNavigation } from "./Navigation";
 
 
 export const Main = () => (
-    <Provider store = {store}>
-        <div>{/*Dashboaard Goes here*/}
-            <ConnectedDashBoard/>
-            
+    <Router history={history}>
+        <Provider store = {store}>
+            <div>{/*Dashboaard Goes here*/}
+                {/* <ConnectedDashBoard/> */}
+                <ConnectedNavigation/>
+                <Route
+                    exact 
+                    path="/dashboard"
+                    render={()=>(<ConnectedDashBoard/>)}
+                    
+                />
+                
 
-        </div>
-    </Provider>
+            </div>
+        </Provider>
+    </Router>
+    
 )
