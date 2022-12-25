6a0d16bb4ace7640985991fd730d858df74db091
Saida Mahmood <saida.mahmood04@gmail.com>
2022-12-25 23:30:15 +0000

    add mutations

--- src/app/components/TaskList.jsx
+++ src/app/components/TaskList.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {connect } from "react-redux";
+import { requestTaskCreation } from "../store/mutations";
 
 export const TaskList = ({tasks,name,id,createNewTask}) => (
     <div>
@@ -29,6 +30,7 @@ const mapDispatchToProps = (dispatch, ownProps)=>{
     return{
         createNewTask(id){
             console.log("create new task....",id);
+            dispatch(requestTaskCreation(id));
         }
 
     }
