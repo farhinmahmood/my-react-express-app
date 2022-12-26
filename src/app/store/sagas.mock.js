58fef47492a835ff8b7b3325634c474fc01083d6
Saida Mahmood <saida.mahmood04@gmail.com>
2022-12-26 22:33:27 +0000

    adding new tasks demo

--- src/app/store/sagas.mock.js
+++ src/app/store/sagas.mock.js
@@ -4,12 +4,17 @@ import{
     select
 } from 'redux-saga/effects'
 
-import * as mutations from './mutations.js'
-import uuid from 'uuid';
+import * as mutations from './mutations'
+import { v4 as uuidv4 } from 'uuid';
+
 
 export function* taskCreationSaga(){
     while(true){
         const {groupID} = yield take(mutations.REQUEST_TASK_CREATION);
+        const ownerID = 'U1';
+        const taskID = uuidv4();
+        ;
+        yield put(mutations.createTask(taskID,groupID,ownerID));
         console.log("got group ID",groupID);
     }
 }
